18bbeee4b31eeaf0bfa329de6fb2b60040b28262
Vert <mickyvert@gmail.com>
2024-10-29 10:13:33 +0300

    index.php modified

--- homeworks2/exercise-02/index.php
+++ homeworks2/exercise-02/index.php
@@ -3,9 +3,9 @@
 $a = 6;
 $b = 0;
 if ($b === 0){
-	echo "Делить на 0 нельзя";
+echo "Делить на 0 нельзя";
 }
-if (is_int($a) & is_int($b)) {
+elseif (is_int($a) & is_int($b)) {
 echo $a / $b;
 } else {
 echo "Введите, пожалуйста, число";
