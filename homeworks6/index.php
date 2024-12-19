3ec66e18d56906e542a30c024ed5063ed28cac5b
vert4 <vert4@mail.ru>
2024-12-19 16:12:38 +0300

    Create index.php

---
+++ homeworks6/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// вывод списка ссылок
+echo "<ul>";
+echo "<li>";
+echo " <a href = ";
+echo "'index2.php'>";
+echo "index2 (404)</a>";
+echo "</li>";
+echo "<li>";
+echo " <a href = ";
+echo "'index3.php?param=text/plain'>";
+echo "index3 txt</a>";
+echo "</li>";
+echo "<li>";
+echo " <a href = ";
+echo "'index4.php'>";
+echo "index4</a>";
+echo "</li>";
+echo "<li>";
+echo " <a href = ";
+echo "'index5.php'>";
+echo "index5</a>";
+echo "</li>";
+echo "</ul>";
